04fe778c1b680df1092b05d9a00e304b732f1063
lehieu1909 <87935396+lehieu1909@users.noreply.github.com>
2021-12-19 23:03:39 +0700

    Add files via upload

---
+++ BT.js
@@ -0,0 +1,93 @@
+// Bài 1: Thiết lập lớp PhanSo để biểu diễn khái niệm phân số với hai thành phần dữ liệu tử số, mẫu số và các hàm thành phần cộng, trừ, nhân, chia hai phân số, các hàm thành phần xuất, nhập, định giá trị cho phân số. Viết chương trình cho phép nhập vào hai phân số, in ra kết quả các phép toán cộng, trừ, nhân, chia hai phân số kể trên.
+
+// class PhanSo {
+    //     constructor(tuSo, mauSo) {
+    //       this.tuSo = tuSo;
+    //       this.mauSo = mauSo;
+    //     }
+
+// cong(phanSo1, phanSo2) {
+
+        //       this.tuSo = phanSo1.tuSo * phanSo2.mauSo + phanSo2.tuSo * phanSo1.mauSo;
+        //       this.mauSo = phanSo1.mauSo * phanSo2.mauSo;
+        //     }
+
+        // toString() {
+            //         console.log(`
+            //           Tử số: ${this.tuSo}
+            //           Mẫu số: ${this.mauSo}`);
+            //       }
+            //     }
+          
+// tru(phanSo1, phanSo2) {
+
+        //       this.tuSo = phanSo1.tuSo * phanSo2.mauSo - phanSo2.tuSo * phanSo1.mauSo;
+        //       this.mauSo = phanSo1.mauSo * phanSo2.mauSo;
+        //     }
+
+        // toString() {
+            //         console.log(`
+            //           Tử số: ${this.tuSo}
+            //           Mẫu số: ${this.mauSo}`);
+            //       }
+            //     }
+          
+// nhan(phanSo1, phanSo2) {
+            
+        //       this.tuSo = phanSo1.tuSo * phanSo2.tuSo;
+        //       this.mauSo = phanSo1.mauSo * phanSo2.mauSo;
+        //     }
+
+        // toString() {
+            //         console.log(`
+            //           Tử số: ${this.tuSo}
+            //           Mẫu số: ${this.mauSo}`);
+            //       }
+            //     }
+          
+// chia(phanSo1, phanSo2) {
+
+        //       this.tuSo = phanSo1.tuSo * phanSo2.mauSo;
+        //       this.mauSo = phanSo1.mauSo * phanSo2.tuSo;
+        //     }
+
+        // toString() {
+            //         console.log(`
+            //           Tử số: ${this.tuSo}
+            //           Mẫu số: ${this.mauSo}`);
+            //       }
+            //     }
+        
+
+// Bài 2:
+
+// Xây dựng lớp Candidate (Thí sinh) gồm các thuộc tính: mã, tên, ngày tháng năm sinh, điểm thi Toán, Văn, Anh và các phương thức cần thiết.
+
+// Xây dựng lớp TestCandidate để kiểm tra lớp trên:
+
+// – Nhập vào n thí sinh (n do người dùng nhập)
+
+// – In ra thông tin về các thí sinh có tổng điểm lớn hơn 15
+
+// class Candidate {
+    //     constructor(code, name, DateOfBirth, math, literature, english) {
+    //       this.code = code;
+    //       this.name = name;
+    //       this.DateOfBirth = DateOfBirth;
+    //       this.math = math;
+    //       this.literature = literature;
+    //       this.english = english;
+    //     }
+
+// toString() {
+//     console.log(`
+//        code: ${this.code}
+//        name: ${this.name}
+//        DateOfBirth: ${this.DateOfBirth}
+//        math: ${this.math}
+//        literature: ${this.literature}
+//        english: ${this.english}
+//           `);
+//     }
+//   }
+
